f857167a839d4604b4c86103efef69beeec64974
samnyan <4137880+samnyan@users.noreply.github.com>
2026-09-20 12:44:25 +0800

    Import CIL body type for structural repository restore

--- Extensions/dnSpy.AsmEditor/ILPatch/ILPatchRepositoryWindow.cs
+++ Extensions/dnSpy.AsmEditor/ILPatch/ILPatchRepositoryWindow.cs
@@ -27,6 +27,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
 using dnlib.DotNet;
+using dnlib.DotNet.Emit;
 using dnSpy.AsmEditor.Commands;
 using dnSpy.AsmEditor.UndoRedo;
 using dnSpy.Contracts.App;
